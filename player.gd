28f8a37fc26fa5799ce9f7e0363870f5f8a199b7
Kirill Ibragimov <hexchap@gmail.com>
2025-06-21 02:39:44 +0300

    Add binding mod for vive cosmos

--- player.gd
+++ player.gd
@@ -7,4 +7,5 @@ func _ready() -> void:
 	_left_controller = XRTools.find_xr_child(self, "LeftHand", "XRController3D")
 	_right_controller = XRTools.find_xr_child(self, "RightHand", "XRController3D")
 	
-	_left_controller.trigger_haptic_pulse("haptic", 0.0, 0.8, 5, 0.0)
+	_left_controller.trigger_haptic_pulse("haptic", 0.0, 0.8, 120, 0.0)
+	_right_controller.trigger_haptic_pulse("godot/haptic", 0.0, 0.8, 120, 0.0)
