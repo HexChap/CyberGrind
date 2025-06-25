e1d50487613e73a94fa5830e4c4b0b5672d376fb
Kirill Ibragimov <hexchap@gmail.com>
2025-06-25 11:56:46 +0300

    add haptic to analysis minigame

--- game/items/hands/hand.gd
+++ game/items/hands/hand.gd
@@ -1,7 +1,7 @@
 extends XRController3D
 
-signal hand_grabbed
-signal hand_released
+signal hand_grabbed(controller: XRController3D)
+signal hand_released(controller: XRController3D)
 
 func _ready():
 	button_pressed.connect(_on_button_pressed)
@@ -9,11 +9,11 @@ func _ready():
 
 func _on_button_pressed(name):
 	if name == "grip_click":
-		emit_signal("hand_grabbed")
+		emit_signal("hand_grabbed", self)
 
 func _on_input_float_changed(name, value):
 	if name == "grip_click":
 		if value > 0.1:
-			emit_signal("hand_grabbed")
+			emit_signal("hand_grabbed", self)
 		else:
-			emit_signal("hand_released")
+			emit_signal("hand_released", self)
